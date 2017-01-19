b680fb99081424a0b1beb1509a06aa08c849b393
Raafe Karim Khan <Raafe@Raafe.local>
2017-01-19 04:21:40 -0500

    Adding the folder

---
+++ Independent_Analysis/Rashad.py
@@ -0,0 +1,34 @@
+#Rashad's First Python Program
+# a='Hello World'
+# b=' My name is Ayra'
+# print(str(a)+str(b))
+
+# a=input("Enter your name")
+
+# a=1+2+3+4+5
+# b=6+7+8+9+10
+# print(a+b)
+
+# a=0
+# b=0
+# for i in range(0,10):
+#     a=i+1
+#     b=a+b
+#
+# print(b)
+
+# a=input('Enter your name')
+# if a=='Rashad':
+#     print('Hello XBOX')
+# else:
+#     print("Wrong Password")
+
+# a=0
+# b=0
+# while a<=9:
+#     a=a+1
+#     b=a+b
+# print(b)
+
+
+
